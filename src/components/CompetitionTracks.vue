e4059532c77c88fcaf9f4a62d62058fec615b532
KyNgo09 <kyb2207535@student.ctu.edu.vn>
2025-10-29 12:50:52 +0700

    add hover effect for CompetitionTrack Cards

--- src/components/CompetitionTracks.vue
+++ src/components/CompetitionTracks.vue
@@ -73,14 +73,15 @@ function onLeave() {
     </div>
 
     <Carousel @init-api="onInit" :opts="{ align: 'center' }" class="relative max-w-6xl mx-auto">
-      <CarouselContent class="gap-4 py-6 md:px-14 px-6">
+      <CarouselContent class="gap-4 py-6 md:px-14 px-6 pb-12">
         <CarouselItem v-for="(track, i) in tracks" :key="track.id" class="basis-auto pl-0">
           <Card @mouseenter="onHover(i)" @mouseleave="onLeave()" :class="[
             'transition-all duration-300 ease-in-out mx-3 sm:mx-2',
             'w-[19rem] sm:w-72 md:w-80 h-92 sm:h-80 md:h-80',
+            'hover:shadow-xl hover:shadow-primary/30',
             selectedIndex !== null
               ? (i === selectedIndex
-                ? 'scale-100 ring-2 ring-primary-500 shadow-lg dark:shadow-black/50 opacity-100 filter-none'
+                ? 'scale-100 ring-2 ring-primary-500 shadow-xl shadow-primary/30 opacity-100 filter-none'
                 : 'opacity-40 scale-95 filter blur-sm')
               : (hoverIndex === i
                 ? 'scale-100 ring-2 ring-primary-500 shadow-lg opacity-100 filter-none'
@@ -110,8 +111,7 @@ function onLeave() {
       </CarouselContent>
     </Carousel>
 
-    <!-- controls -->
-    <div class="mt-2 flex justify-center items-center gap-3">
+    <div class="flex justify-center items-center gap-3">
       <Button v-for="(t, idx) in tracks" :key="t.id" variant="ghost" size="sm"
         class="h-10 w-10 rounded-full p-0 flex items-center justify-center" :class="idx === selectedIndex
           ? 'bg-gradient-to-r from-[#D247BF] to-primary text-white shadow-md'
